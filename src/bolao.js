f750c100ad850addf7de06149e7ca4a25415918a
Carlos Rafael Lopes da Silva <carlosrafael.silva@correios.com.br>
2025-08-25 16:11:23 -0400

    1.0

--- src/bolao.js
+++ src/bolao.js
@@ -5,7 +5,7 @@ function Bolao() {
 
     useEffect(() => {
         // fetch('http://127.0.0.1:8000/api/index/')
-        fetch('https://bolao-68c4.onrender.com/api/index')
+        fetch('https://bolao-68c4.onrender.com/api/index/')
             .then(response => response.json())
             .then(data => setMessage(data.message))
             .catch(error => console.error('Error fetching data:', error));
