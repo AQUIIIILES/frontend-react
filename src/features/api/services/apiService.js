9da8909f4ca8e3e78c32bcc790afcc8889040719
Carlos Rafael Lopes da Silva <carlosrafael.silva@correios.com.br>
2025-09-23 17:43:12 -0400

    1.0

--- src/features/api/services/apiService.js
+++ src/features/api/services/apiService.js
@@ -6,15 +6,13 @@ const BASE_URL = import .meta.env.VITE_API_URL; // Para deploy com Vite
 
 export const getHello = async () => {
     //const response = await axios.get(BASE_URL);
-    //const response = await axios.get(`${BASE_URL}/index/`); // Para deploy com Vite
-    const response = await axios.get('/api/index/'); // Para deploy com Vite e proxy no vite.config.js
+    const response = await axios.get(`${BASE_URL}/index/`); // Para deploy com Vite
     return response.data;
 };
 
 export const postHello = async (data) => {
     //const response = await axios.post(BASE_URL, data);
-    //const response = await axios.get(`${BASE_URL}/index/`); // Para deploy com Vite
-    const response = await axios.get('/api/index/'); // Para deploy com Vite e proxy no vite.config.js
+    const response = await axios.get(`${BASE_URL}/index/`); // Para deploy com Vite
     return response.data;
 };
 
